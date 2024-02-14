81cbba0d16fb033eacf4cfe1c2fdbb1fa47430c7
Shahid Sulieman <shahid@Pioneerstech.local>
2024-02-14 16:17:08 +0300

    new 3

--- shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.scss'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes = [];
+  
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+ 
 
 }
