774c9052e9c4bf67b89e5c4fd2e9658e144ffc4b
Shahid Sulieman <shahid@Pioneerstech.local>
2024-02-15 16:26:39 +0300

    happy

--- shopping/src/app/shopping-list/shopping-list.component.ts
+++ shopping/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingrdient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss'
 })
-export class ShoppingListComponent {
-
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingrdient[] = [
+    new Ingrdient('apples' , 5),
+    new Ingrdient('mangos' , 10)
+  ];
+    ngOnInit(): void {
+        
+    }
 }
